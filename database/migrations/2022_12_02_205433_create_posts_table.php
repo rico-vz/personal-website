d2837765c8efa2e24d9fe41521eb3105ba9d3677
Rico van Zelst <ricoz@tuta.io>
2022-12-02 22:41:25 +0100

    🆕 Let's get started

---
+++ database/migrations/2022_12_02_205433_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->text('description');
+            $table->string('slug');
+            $table->string('url');
+            $table->string('cover_image');
+            $table->integer('read_time_in_minutes');
+            $table->date('published_timestamp');
+            $table->integer('comment_count');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
