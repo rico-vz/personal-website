62f9fd0fdbbf966fb950f19e32e4f1b3d0a62a5a
Rico van Zelst <ricoz@tuta.io>
2023-01-15 21:41:13 +0100

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -30,7 +30,7 @@ module.exports = {
 					"error": "#ee99a0",
 				},
 				catLatte: {
-					"primary": "#4956a3",
+					"primary": "#38427d",
 					"secondary": "#7287fd",
 					"accent": "#04a5e5",
 					"neutral": "#dce0e8",
