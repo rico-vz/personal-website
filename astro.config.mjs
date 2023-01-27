51eef6f7f69fa93ce310c916d632dbd932dc6bc9
Rico van Zelst <ricovzelst@pm.me>
2023-01-28 00:23:00 +0100

    add; ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
@@ -11,9 +12,12 @@ import compress from "astro-compress";
 // https://astro.build/config
 import critters from "astro-critters";
 
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ricovz.me',
-  integrations: [mdx(), sitemap(), tailwind(), compress(), critters()]
+  integrations: [mdx(), sitemap(), tailwind(), compress(), critters()],
+  output: 'server',
+  adapter: vercel()
 });
